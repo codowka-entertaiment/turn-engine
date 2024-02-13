2cb4f8190816329290ad3bbda3fc7085ca516606
Qurcaivel <alexinq6@gmail.com>
2024-02-13 22:21:33 +0300

    Add Hexagon shape

--- src/math/Shape.hpp
+++ src/math/Shape.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
+#include "Math.hpp"
 #include "Vector.hpp"
 
 #include <cinttypes>
 #include <expected>
+#include <numeric>
 
 namespace math
 {
     enum class RectangleShapeError : std::uint8_t {
         WrongVertices,
-        NegativeDimensions,
+        WrongDimensions,
     };
 
     template <Numeric T>
@@ -33,7 +35,7 @@ namespace math
 
         constexpr static std::expected<Rectangle<T>, RectangleShapeError> init(Position<T> vertex, T width, T height) {
             if (width <= 0 || height <= 0)
-                return std::unexpected(RectangleShapeError::NegativeDimensions);
+                return std::unexpected(RectangleShapeError::WrongDimensions);
             else
                 return Rectangle(vertex, vertex + Position<T>(width, height));
         }
@@ -45,4 +47,41 @@ namespace math
     };
 
     using RectangleInt = Rectangle<int>;
+
+    enum class HexagonShapeError : std::uint8_t {
+        WrongSide,
+    };
+
+    template <Numeric T>
+    class Hexagon : public Shape<T> {
+    private:
+        Position<T> m_center;
+        T m_side;
+        constexpr Hexagon(Position<T> center, T side) : m_center(center), m_side(side) {}
+
+    public:
+        constexpr static std::expected<Hexagon<T>, HexagonShapeError> init(Position<T> center, T side) {
+            if (side <= 0)
+                return std::unexpected(HexagonShapeError::WrongSide);
+            else
+                return Hexagon(center, side);
+        }
+
+        constexpr bool contains(const Position<T> &position) const override {
+            // check point under/upper sides
+            auto pos = position - m_center;
+
+            constexpr auto const1 = math::sqrt(3);
+            constexpr auto const2 = const1 / 2.0;
+
+            return (pos.y <= +const2 * m_side) &&            // F1
+                   (pos.y <= -const1 * (pos.x - m_side)) &&  // F2
+                   (pos.y >= +const1 * (pos.x - m_side)) &&  // F3
+                   (pos.y >= -const2 * m_side) &&            // F4
+                   (pos.y >= -const1 * (pos.x + m_side)) &&  // F5
+                   (pos.y <= +const1 * (pos.x + m_side));    // F6
+        }
+    };
+
+    using HexagonInt = Hexagon<int>;
 }
