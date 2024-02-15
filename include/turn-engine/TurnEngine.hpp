69607f584dd326e61f01f9c0868aa9b134bb8c94
Kulakovich Daniil <92942037+DanikKul@users.noreply.github.com>
2024-02-15 21:21:48 +0300

    CORE-63: Update CMakeLists.txt and add common public header

---
+++ include/turn-engine/TurnEngine.hpp
@@ -0,0 +1,12 @@
+#ifndef TURN_ENGINE_TURNENGINE_HPP
+#define TURN_ENGINE_TURNENGINE_HPP
+
+#include <turn-engine/Engine.hpp>
+#include <turn-engine/math/Math.hpp>
+#include <turn-engine/math/Shapes.hpp>
+#include <turn-engine/math/Vector.hpp>
+#include <turn-engine/math/shapes/Hexagon.hpp>
+#include <turn-engine/math/shapes/Rectangle.hpp>
+#include <turn-engine/math/shapes/Shape.hpp>
+
+#endif  //TURN_ENGINE_TURNENGINE_HPP
