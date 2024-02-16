57403f8d901d779613792b270143f39c1d85af5e
antoluk <antoluk2003@gmail.com>
2024-02-16 11:33:41 +0300

    move surface constructor to private

--- src/sdl/Surface.hpp
+++ src/sdl/Surface.hpp
@@ -6,6 +6,7 @@
 #include "SDL.h"
 #include "SDL_image.h"
 #include "sdl/Texture.hpp"
+#include "sdl/Window.hpp"
 #include "string_view"
 
 namespace sdl
@@ -14,12 +15,13 @@ namespace sdl
 
     class Surface {
         friend class Texture;
+        friend class Window;
 
     private:
         SDL_Surface *surface;
+        explicit Surface(SDL_Surface *surface);
 
     public:
-        explicit Surface(SDL_Surface *surface);
         static std::expected<Surface, SurfaceError> init(std::string_view filename);
         ~Surface();
     };
