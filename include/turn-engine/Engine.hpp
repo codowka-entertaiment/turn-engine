ce8aef4a89b7a6b44115f81b3485847b29ec53cb
Qurcaivel <alexinq6@gmail.com>
2024-02-19 13:23:43 +0300

    Rename math to geo2d, move Math.hpp to ext

--- include/turn-engine/Engine.hpp
+++ include/turn-engine/Engine.hpp
@@ -1,9 +1,4 @@
 #pragma once
 
-#include <turn-engine/Engine.hpp>
-#include <turn-engine/math/Math.hpp>
-#include <turn-engine/math/Shapes.hpp>
-#include <turn-engine/math/Vector.hpp>
-#include <turn-engine/math/shapes/Hexagon.hpp>
-#include <turn-engine/math/shapes/Rectangle.hpp>
-#include <turn-engine/math/shapes/Shape.hpp>
+#include <turn-engine/ext/Types.hpp>
+#include <turn-engine/geom/Shapes.hpp>
